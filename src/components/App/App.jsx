6bccfcc98014b03d28b0bf4a55338997e90d5361
Mykhailo Kuzmenko <103496282+Mushko2932@users.noreply.github.com>
2023-05-11 15:07:59 +0300

    update App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component, useEffect, useState } from "react";
 import Notiflix from 'notiflix';
 import { GlobalStyle } from "components/GlobalStyle";
 import { ImageGallery } from "components/ImageGallery/ImageGallery";
@@ -9,94 +9,80 @@ import { Button } from "components/Button/Button";
 import { Modal } from "components/Modal/Modal";
 import { Container } from "./App.styled";
 
-export class App extends Component {
-  state = {
-    search: '',
-    images: [],
-    page: 1,
-    total: 1,
-    isLoading: false,
-    showModal: false,
-    error: false,
-  };
+export const App = () => {
+  const [search, setSearch] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [modal, setModal] = useState({largeImageURL: ''});
+  const [error, setError] = useState(false);
 
-  async componentDidUpdate(prevProps, prevState) {
-    const { images, search, page } = this.state;
-    
-    if (prevState.search !== search || prevState.page !== page) {
+  useEffect(() => {
+    async function getPhoto() {
       try {
-        this.setState({ isLoading: true });
+        setIsLoading(true);
         const fetchedImg = await fetchImgList(search, page);
-        this.setState(prevState => ({
-          images: [...images, ...fetchedImg.hits],
-          page: prevState.page,
-          total: fetchedImg.total,
-          isLoading: false,
-        }));
+        setImages(prevState => [...prevState, ...fetchedImg.hits]);
+        setPage(1);
+        setTotal(1);
       } catch (error) {
         console.log('error :>> ', error);
-        this.setState({ error: true, isLoading: false });
+        setError(true);
+        setIsLoading(false);
       }
     }
-  }
+    getPhoto(search);
+  }, [page, search]);
 
-  handleSubmit = searchQuery => {
+  const handleSubmit = searchQuery => {
     const warningMessage = 'Something wrong! Please try again.';
     if (searchQuery.trim() === '') {
       Notiflix.Notify.warning(warningMessage);
       return;
     }
       try {
-        if (searchQuery === this.state.search) {
+        if (searchQuery === search) {
           return;
         }
-        this.setState({ search: searchQuery, page: 1, images: [] });
+        setSearch(searchQuery);
+        setPage(1);
+        setImages([]);
       } catch (error) {
         console.log('error :>> ', error);
       } 
   };
 
-  loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
+  const handleImgClick = largeImageURL => {
+    setModal(prevState => ({ ...prevState, largeImageURL }));
+    toggleModal();
+  };
+
+  const loadMore = () => {
+    setPage(prevState => prevState.page + 1,);
+  };
+
+  const toggleModal = () => {
+    setShowModal(prevState => ({
+      ...prevState,
+      showModal: !prevState.showModal,
     }));
   };
 
-  toggleModal = (largeImageURL, alt) => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-      largeImageURL,
-      alt,
-    }));
-  };
-
-  render() {
-    const {
-      isLoading,
-      images,
-      error,
-      showModal,
-      largeImageURL,
-      alt,
-      total,
-      page,
-    } = this.state;
     return (
       <Container>
-        <SearchBar onSubmit={this.handleSubmit} />
+        <SearchBar onSubmit={handleSubmit} />
         {images.length > 0 && (
-          <ImageGallery items={images} toggleModal={this.toggleModal} />
+          <ImageGallery items={images} toggleModal={handleImgClick} />
         )}
         {isLoading && <Loader />}
         {error && <p>Help...</p>}
-        {total / 12 > page && <Button onClick={this.loadMore} />}
+        {total / 12 > page && <Button onClick={loadMore} />}
         {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <img src={largeImageURL} alt={alt} />
-          </Modal>
+          <Modal onClose={toggleModal} largeImageURL={modal.largeImageURL} />
         )}
         <GlobalStyle />
       </Container>
     );
-  }
 };
